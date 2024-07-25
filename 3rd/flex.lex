a7163449ddec00f5277916e6113d002154ffc747
chrisgalanis <chrisgalan2004@gmail.com>
2024-07-25 14:07:09 +0300

    3rd/ (Half of the 1)/3

--- 3rd/flex.lex
+++ 3rd/flex.lex
@@ -1,7 +1,6 @@
 %{
 #include<stdio.h>
 #include "parser.tab.h"
-
 %}
 
 INT "int"
--- 3rd/lex.yy.c
+++ 3rd/lex.yy.c
@@ -531,9 +531,8 @@ char *yytext;
 #line 2 "flex.lex"
 #include<stdio.h>
 #include "parser.tab.h"
-
+#line 535 "lex.yy.c"
 #line 536 "lex.yy.c"
-#line 537 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -750,9 +749,9 @@ YY_DECL
 		}
 
 	{
-#line 30 "flex.lex"
+#line 29 "flex.lex"
 
-#line 756 "lex.yy.c"
+#line 755 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -811,206 +810,206 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 31 "flex.lex"
+#line 30 "flex.lex"
 {printf("\n { \n"); return CURLY_BRACKET_LEFT;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 32 "flex.lex"
+#line 31 "flex.lex"
 {printf("\n } \n");return CURLY_BRACKET_RIGHT;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 33 "flex.lex"
+#line 32 "flex.lex"
 {printf("\n ( \n");return BRACKET_LEFT;}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 34 "flex.lex"
+#line 33 "flex.lex"
 {printf("\n ) \n");return BRACKET_RIGHT;}
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 35 "flex.lex"
+#line 34 "flex.lex"
 {return EQUAL_SIGN;}
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 36 "flex.lex"
+#line 35 "flex.lex"
 {printf("\n ; \n");return SEMICOLON;}
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 37 "flex.lex"
+#line 36 "flex.lex"
 {return SINGLE_MARK;}
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 38 "flex.lex"
+#line 37 "flex.lex"
 {return COLON;}
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 39 "flex.lex"
+#line 38 "flex.lex"
 {return COMMA;}
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 40 "flex.lex"
+#line 39 "flex.lex"
 {return DOT;}
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 43 "flex.lex"
+#line 42 "flex.lex"
 {printf("\n FOR \n"); return FOR;}
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 44 "flex.lex"
+#line 43 "flex.lex"
 {printf("\n DO \n"); return DO;}
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 45 "flex.lex"
+#line 44 "flex.lex"
 {return WHILE;}
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 47 "flex.lex"
+#line 46 "flex.lex"
 {return SWITCH;}
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 48 "flex.lex"
+#line 47 "flex.lex"
 {return CASE; }
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 49 "flex.lex"
+#line 48 "flex.lex"
 {return DEFAULT;}
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 51 "flex.lex"
+#line 50 "flex.lex"
 {return BREAK;}
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 52 "flex.lex"
+#line 51 "flex.lex"
 {return RETURN;}
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 54 "flex.lex"
+#line 53 "flex.lex"
 { return CONDITION_SYMBOL; }
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 55 "flex.lex"
+#line 54 "flex.lex"
 {return BOOL_SYMBOL;}
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 56 "flex.lex"
+#line 55 "flex.lex"
 {return INCREAMENT_DECREAMENT;}
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 57 "flex.lex"
+#line 56 "flex.lex"
 {return LOOP_STEP;}
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 59 "flex.lex"
+#line 58 "flex.lex"
 {return VOID;}
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 60 "flex.lex"
+#line 59 "flex.lex"
 {return INT;}
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 61 "flex.lex"
+#line 60 "flex.lex"
 {return CHAR;}
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 62 "flex.lex"
+#line 61 "flex.lex"
 {return STRING;}
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 63 "flex.lex"
+#line 62 "flex.lex"
 {return DOUBLE;}
 	YY_BREAK
 case 28:
 YY_RULE_SETUP
-#line 64 "flex.lex"
+#line 63 "flex.lex"
 {return BOOLEAN;}
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 66 "flex.lex"
+#line 65 "flex.lex"
 {printf("\nPUBLIC\n"); return PUBLIC;}
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 67 "flex.lex"
+#line 66 "flex.lex"
 {printf("\nPRIVATE\n"); return PRIVATE;}
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 68 "flex.lex"
+#line 67 "flex.lex"
 {printf("\nCLASS\n");return CLASS;}
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 69 "flex.lex"
+#line 68 "flex.lex"
 {return NEW;}
 	YY_BREAK
 case 33:
 YY_RULE_SETUP
-#line 70 "flex.lex"
+#line 69 "flex.lex"
 {printf("\nCLASS NAME\n"); return CLASS_NAME;}
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 72 "flex.lex"
+#line 71 "flex.lex"
 { yylval.ival = atoi(yytext); return INT_VALUE;}
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 73 "flex.lex"
+#line 72 "flex.lex"
 { yylval.cval = yytext[1]; return CHAR_VALUE;}
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 74 "flex.lex"
+#line 73 "flex.lex"
 { yylval.sval = strdup(yytext); return STRING_VALUE;}
 	YY_BREAK
 case 37:
 YY_RULE_SETUP
-#line 75 "flex.lex"
+#line 74 "flex.lex"
 { yylval.sval = strdup(yytext); return BOOLEAN_VALUE;}
 	YY_BREAK
 case 38:
 YY_RULE_SETUP
-#line 76 "flex.lex"
+#line 75 "flex.lex"
 { yylval.sval = strdup(yytext); printf("\nVAR NAME\n");return VAR_NAME;}
 	YY_BREAK
 case 39:
 /* rule 39 can match eol */
 YY_RULE_SETUP
-#line 77 "flex.lex"
+#line 76 "flex.lex"
 {}
 	YY_BREAK
 case 40:
 YY_RULE_SETUP
-#line 78 "flex.lex"
+#line 77 "flex.lex"
 ECHO;
 	YY_BREAK
-#line 1014 "lex.yy.c"
+#line 1013 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -2015,7 +2014,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 78 "flex.lex"
+#line 77 "flex.lex"
 
 
 
